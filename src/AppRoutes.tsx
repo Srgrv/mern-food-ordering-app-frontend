26f2c62b04bf971a1ce11eea7ef179616acf9e03
Sergey Georgiev <sgrgv_art@mail.ru>
2024-05-25 15:59:20 +0300

    Этот фрагмент кода определяет маршрут в React-приложении с использованием компонента Route из библиотеки React Router

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -8,6 +8,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = () => {
   return (
@@ -31,6 +32,14 @@ const AppRoutes = () => {
           }
         />
       </Route>
+      <Route
+        path="/manage-restaurant"
+        element={
+          <Layout>
+            <ManageRestaurantPage />
+          </Layout>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
