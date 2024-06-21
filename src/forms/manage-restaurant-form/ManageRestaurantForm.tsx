7ad711220ac8d4f556d94922e7668aee94bbead3
Sergey Georgiev <sgrgv_art@mail.ru>
2024-06-21 12:16:50 +0300

    - Исправлены ошибки - Определена схема данных, которая может использоваться для валидации объектов в JavaScript или TypeScript. Она требует, чтобы либо imageUrl (строка URL изображения), либо imageFile (экземпляр класса File) были предоставлены в объекте данных. Если это условие не выполняется, будет сгенерирована ошибка с заданным сообщением -  Сконвертированы данные из объекта RestaurantFormData в объект FormData, подготавливая их для отправки на сервер

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -2,7 +2,7 @@ import { Form } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
-import z from "zod";
+import { z } from "zod";
 
 //forms
 import DetailsSection from "./DetailsSection";
@@ -15,36 +15,43 @@ import { Button } from "@/components/ui/button";
 
 // Этот код использует библиотеку zod для определения схемы данных и валидации значений формы согласно этой схеме. Он помогает обеспечить правильный ввод данных пользователем и предотвращает отправку некорректных данных на сервер.
 
-const formSchema = z.object({
-  restaurantName: z.string({
-    required_error: "restaurant name is required",
-  }),
-  city: z.string({
-    required_error: "city name is required",
-  }),
-  country: z.string({
-    required_error: "country name is required",
-  }),
-  // метод z.coerce.number используется для преобразования значения в число.
-  deliveryPrice: z.coerce.number({
-    required_error: "delivery price is required",
-    invalid_type_error: "must be a valid number",
-  }),
-  estimatedDeliveryTime: z.coerce.number({
-    required_error: "estimated delivery time is required",
-    invalid_type_error: "must be a valid number",
-  }),
-  cuisins: z.array(z.string()).nonempty({
-    message: "please select at least one item",
-  }),
-  menuItems: z.array(
-    z.object({
-      name: z.string().min(1, "name is required"),
-      price: z.coerce.number().min(1, "price is required"),
-    })
-  ),
-  imageUrl: z.instanceof(File, { message: "image is required" }),
-});
+const formSchema = z
+  .object({
+    restaurantName: z.string({
+      required_error: "restaurant name is required",
+    }),
+    city: z.string({
+      required_error: "city name is required",
+    }),
+    country: z.string({
+      required_error: "country name is required",
+    }),
+    // метод z.coerce.number используется для преобразования значения в число.
+    deliveryPrice: z.coerce.number({
+      required_error: "delivery price is required",
+      invalid_type_error: "must be a valid number",
+    }),
+    estimatedDeliveryTime: z.coerce.number({
+      required_error: "estimated delivery time is required",
+      invalid_type_error: "must be a valid number",
+    }),
+    cuisins: z.array(z.string()).nonempty({
+      message: "please select at least one item",
+    }),
+    menuItems: z.array(
+      z.object({
+        name: z.string().min(1, "name is required"),
+        price: z.coerce.number().min(1, "price is required"),
+      })
+    ),
+    // imageUrl: z.instanceof(File, { message: "image is required" }),
+    imageUrl: z.string().optional(),
+    imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+  })
+  .refine((data) => data.imageUrl || data.imageFile, {
+    message: "Either image URL or image File must be provided",
+    path: ["imageFile"],
+  });
 
 // types
 
@@ -74,6 +81,40 @@ const ManageRestaurantForm: React.FC<TProps> = ({ onSave, isLoading }) => {
 
   const onSubmit = (formDataJson: RestaurantFormData) => {
     // TODO - convert formDataJson to a new FormData object
+
+    console.log(formDataJson);
+
+    const formData = new FormData();
+
+    formData.append("restaurantName", formDataJson.restaurantName);
+
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append(
+      "deliveryPrice",
+      (formDataJson.deliveryPrice * 100).toString()
+    );
+    formData.append(
+      "estimatedDeliveryTime",
+      formDataJson.estimatedDeliveryTime.toString()
+    );
+    formDataJson.cuisins.forEach((cuisine, index) => {
+      formData.append(`cuisins[${index}]`, cuisine);
+    });
+    formDataJson.menuItems.forEach((menuItem, index) => {
+      formData.append(`menuItems[${index}][name]`, menuItem.name);
+      formData.append(
+        `menuItems[${index}][price]`,
+        (menuItem.price * 100).toString()
+      );
+    });
+    // formData.append(`imageUrl`, formDataJson.imageUrl);
+
+    if (formDataJson.imageFile) {
+      formData.append(`imageFile`, formDataJson.imageFile);
+    }
+
+    onSave(formData);
   };
 
   return (
@@ -90,7 +131,7 @@ const ManageRestaurantForm: React.FC<TProps> = ({ onSave, isLoading }) => {
         <MenuSection />
         <Separator />
         <ImageSection />
-        {isLoading ? <LoadingButton /> : <Button type="submit">Sumbit</Button>}
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
       </form>
     </Form>
   );
