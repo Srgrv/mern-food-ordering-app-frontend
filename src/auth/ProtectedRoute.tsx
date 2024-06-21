0574d26af44bb809a7aadd19171699e955f71113
Sergey Georgiev <sgrgv_art@mail.ru>
2024-06-21 14:12:16 +0300

    Добавлен isLoading и разграничены условия отображения isAuthenticated

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -3,8 +3,16 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute: React.FC = () => {
-  const { isAuthenticated } = useAuth0();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
