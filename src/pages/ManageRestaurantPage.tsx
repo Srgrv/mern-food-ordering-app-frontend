4224677f024158b825a8b62905bc99fc1b2f85a8
Sergey Georgiev <sgrgv_art@mail.ru>
2024-06-22 11:05:56 +0300

    Использован хук для обновления ресторана

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -4,21 +4,27 @@ import React from "react";
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 
 //components
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage: React.FC = () => {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+
+  const isEditing = !!restaurant;
 
   return (
     <div>
       <ManageRestaurantForm
         restaurant={restaurant}
-        onSave={createRestaurant}
-        isLoading={isLoading}
+        onSave={isEditing ? updateRestaurant : createRestaurant}
+        isLoading={isCreateLoading || isUpdateLoading}
       />
     </div>
   );
