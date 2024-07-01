1f7a6aeea94f0b2e27b13c456056c26cc269add3
Sergey Georgiev <sgrgv_art@mail.ru>
2024-07-01 11:45:21 +0300

    Добавлена функция для перенаправления пользователя на страницу поиска с указанным поисковым запросом Импортирован SearhBar и в него передена в качестве пропса функция выше

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 //images
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
 
+// components and types
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  // Эта функция принимает данные формы (searchFormValues), которые автоматически передаются из компонента SearchBar при отправке формы. Она использует navigate для перенаправления пользователя на страницу поиска с указанным поисковым запросом.
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    //  Навигация к новому URL
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
-      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-500">
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Поиск по городу"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
