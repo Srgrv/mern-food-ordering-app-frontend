e5eced2d242ed022ae517903e38a55f1259e2f79
Sergey Georgiev <sgrgv_art@mail.ru>
2024-07-01 13:19:18 +0300

    - Добавлена обработка состояний загрузки и ошибок - Улучшены ключи в списках и предоставлены более безопасный доступ к данным. Теперь компонент сообщает пользователю о текущем состоянии запроса и отображает результаты поиска ресторанов, если они доступны.

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,24 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 const SearchPage: React.FC = () => {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
 
-  return <span>По {city} ничего не найдено</span>;
+  return (
+    <span>
+      Поиск по городу {city}
+      <span>
+        {results?.data.map((restaurant, index) => (
+          <span key={`${restaurant}${index}`}>
+            {" "}
+            найдено - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 };
 
 export default SearchPage;
