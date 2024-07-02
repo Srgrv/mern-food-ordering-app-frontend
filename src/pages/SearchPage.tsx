682da7e52985c125207deee7b439bc4730f1ccd9
Sergey Georgiev <sgrgv_art@mail.ru>
2024-07-02 15:29:32 +0300

    Страница SearchPage получает данные о ресторанах из API и отображает их с помощью компонентов SearchResultCard и SearchResultInfo. Также он обрабатывает состояния загрузки и отсутствие результатов

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,23 +1,39 @@
-import { useSearchRestaurants } from "@/api/RestaurantApi";
 import React from "react";
 import { useParams } from "react-router-dom";
 
+// components
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
+
+// api hook
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+
 const SearchPage: React.FC = () => {
   const { city } = useParams();
-  const { results } = useSearchRestaurants(city);
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  // Обработка состояния загрузки: Если данные еще загружаются, отображается сообщение "Загрузка...".
+  if (isLoading) {
+    <span>Загрузка...</span>;
+  }
+
+  // Обработка отсутствия результатов: Если данные не найдены или параметр city отсутствует, отображается сообщение "Результатов не найдено".
+  if (!results?.data || !city) {
+    return <span>Результатов не найдено</span>;
+  }
+
+  // Отображение результатов поиска: Если данные найдены, отображается информация о результатах поиска с помощью компонента SearchResultInfo.
 
   return (
-    <span>
-      Поиск по городу {city}
-      <span>
-        {results?.data.map((restaurant, index) => (
-          <span key={`${restaurant}${index}`}>
-            {" "}
-            найдено - {restaurant.restaurantName}, {restaurant.city}
-          </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisins-list">Вставьте сюда кухни</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
-      </span>
-    </span>
+      </div>
+    </div>
   );
 };
 
