7b1282b9448c4f23876e93d67f81cfefda3cbb8f
Sergey Georgiev <sgrgv_art@mail.ru>
2024-07-21 15:09:38 +0300

    Изменения: - Добавлен импорт типов CartItem, MenuItem и компонентов OrderSummary, Card - Добавление состояния cartItems с помощью useState - Добавлена функция addToCart, которая принимает menuItem типа MenuItem - Использование addToCart в компоненте Menu - Рендеринг компонента OrderSummary

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,15 +1,67 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 //components
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import RestaurantInto from "@/components/RestaurantInto";
 import Menu from "@/components/Menu";
+import OrderSummary from "@/components/OrderSummary";
+import { Card } from "@/components/ui/card";
+
+//types
+import { MenuItem } from "@/types";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage: React.FC = () => {
   const { restaurantId } = useParams();
   const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (menuItem: MenuItem): void => {
+    setCartItems((prevCartItems) => {
+      // 1. Поиск существующего элемента в корзине:
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+
+      let updatedCartItems;
+
+      // 2. Обновление состояния корзины:
+
+      if (existingCartItem) {
+        // Если existingCartItem определен (т.е. товар уже есть в корзине), мы используем метод массива map, чтобы создать новый массив updatedCartItems. В этом новом массиве мы проверяем каждый элемент:
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? // Если _id элемента cartItem совпадает с _id товара menuItem, то мы создаем новый объект { ...cartItem, quantity: cartItem.quantity + 1 }, который представляет собой копию текущего элемента с увеличенным на 1 количеством (quantity).
+              { ...cartItem, quantity: cartItem.quantity + 1 }
+            : // Если _id не совпадает, мы просто добавляем текущий cartItem без изменений в новый массив.
+              cartItem
+        );
+      } else {
+        // Если existingCartItem не найден (т.е. товара еще нет в корзине), мы создаем новый массив updatedCartItems, который включает все элементы из prevCartItems, а также добавляем новый объект для menuItem с начальным количеством quantity равным 1.
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      // Функция setCartItems, предположительно, используется в контексте хуков React для обновления состояния cartItems. Мы возвращаем updatedCartItems из колбэк-функции, переданной setCartItems. Это обновленное состояние cartItems будет затем использовано для отображения или дальнейшей обработки в вашем приложении.
+      return updatedCartItems;
+    });
+  };
 
   if (isLoading || !restaurant) {
     return "Загрузка...";
@@ -29,8 +81,17 @@ const DetailPage: React.FC = () => {
           <RestaurantInto restaurant={restaurant} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {restaurant.menuItems.map((menuItem, index) => (
-            <Menu key={`${menuItem}_${index}`} menuItem={menuItem} />
+            <Menu
+              key={`${menuItem}_${index}`}
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
+        </div>
+        <div className="">
+          <Card>
+            <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
