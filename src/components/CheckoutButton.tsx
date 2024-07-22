70b40049ab1f7dc4a359c841562da0e6078a7373
Sergey Georgiev <sgrgv_art@mail.ru>
2024-07-22 13:40:24 +0300

    Изменения: - Добавлен хук для получения данных пользователя - Определен тип пропсов для компонента - Использован хук выше для загрузки данных. Он предоставляет информацию о текущем пользователе currentUser и статус загрузки isGetUserLoading - Добавлено условие !currentUser для отображения кнопки входа - Отображение основного контента

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -5,8 +5,27 @@ import { useAuth0 } from "@auth0/auth0-react";
 //components
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import {
+  UserFormData,
+  UserProfileForm,
+} from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton: React.FC = () => {
+// types
+type TProps = {
+  onCheckout: (UserFormData: UserFormData) => void;
+  disabled: boolean;
+};
+
+const CheckoutButton: React.FC<TProps> = ({ onCheckout, disabled }) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
@@ -14,6 +33,8 @@ const CheckoutButton: React.FC = () => {
   } = useAuth0();
 
   const { pathname } = useLocation();
+
+  const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
 
   // /detail/2342342 - pathname
   const onLogin = async () => {
@@ -24,7 +45,7 @@ const CheckoutButton: React.FC = () => {
     });
   };
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated || !currentUser) {
     return (
       <Button className="bg-orange-500 flex-1" onClick={onLogin}>
         Войдите, чтобы оформить заказ
@@ -35,6 +56,33 @@ const CheckoutButton: React.FC = () => {
   if (isAuthLoading) {
     return <LoadingButton />;
   }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button disabled={disabled} className="bg-orange-500 flex-1">
+          Перейти к оформлению заказа
+        </Button>
+      </DialogTrigger>
+      <DialogContent
+        className="max-w-[425px] md:min-w-[700px] bg-gray-50"
+        aria-labelledby="dialog-title"
+      >
+        <DialogHeader>
+          {/* <DialogTitle>Перейти к оформлению заказа</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription> */}
+
+          <UserProfileForm
+            currentUser={currentUser}
+            onSave={onCheckout}
+            isLoading={isGetUserLoading}
+          />
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CheckoutButton;
